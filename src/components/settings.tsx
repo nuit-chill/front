a88da06048ed9712894de68ab03abb53534ad3c2
Muriel Paraire <72733662+MurielParaire@users.noreply.github.com>
2023-12-08 02:39:41 +0100

    fix(translation): added settings translation

--- src/components/settings.tsx
+++ src/components/settings.tsx
@@ -3,18 +3,21 @@ import { useDispatch } from 'react-redux'
 import { decrease, increase } from "../slices/font-size";
 import SelectLanguage from "./select_language";
 import { Divider } from '@chakra-ui/react'
+import { useTranslation } from 'react-i18next'
+
 
 export const Settings = () => {
+    const { t } = useTranslation("homepage");
     const dispatch = useDispatch();
 
     return <PopoverContent style={{margin: '5px'}}>
     <PopoverArrow />
     <PopoverCloseButton />
-    <PopoverHeader><Heading as='h1' size='md'>Paramètres</Heading></PopoverHeader>
+    <PopoverHeader><Heading as='h1' size='md'>{t("settings.title")}</Heading></PopoverHeader>
     <PopoverBody>
         <SelectLanguage />
         <Divider className="marginBox" />
-        <p>Changer la taille de la police&nbsp;:</p>
+        <p>{t("settings.fontsize")}&nbsp;:</p>
         <Button className="marginBox" onClick={() => dispatch(increase())}>A+</Button>
         <Button className="marginBox" onClick={() => dispatch(decrease())}>A-</Button>
     </PopoverBody>
