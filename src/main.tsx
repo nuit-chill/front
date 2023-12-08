ba23d5f00419a78a0f2e021205059dc874c1e865
Muriel Paraire <72733662+MurielParaire@users.noreply.github.com>
2023-12-08 07:25:30 +0100

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -16,6 +16,18 @@ import homepage_fr from './languages/fr/homepage.json'
 import homepage_de from './languages/de/homepage.json'
 import i18next from 'i18next'
 import { I18nextProvider } from 'react-i18next'
+import company_angel_en from './languages/en/company_angel.json'
+import company_demon_en from './languages/en/company_demon.json'
+import person_angel_en from './languages/en/person_angel.json'
+import person_demon_en from './languages/en/person_demon.json'
+import company_angel_fr from './languages/fr/company_angel.json'
+import company_demon_fr from './languages/fr/company_demon.json'
+import person_angel_fr from './languages/fr/person_angel.json'
+import person_demon_fr from './languages/fr/person_demon.json'
+import company_angel_de from './languages/de/company_angel.json'
+import company_demon_de from './languages/de/company_demon.json'
+import person_angel_de from './languages/de/person_angel.json'
+import person_demon_de from './languages/de/person_demon.json'
 
 i18next.init({
   interpolation: { escapeValue: false },
@@ -23,12 +35,24 @@ i18next.init({
   resources: {
    en: {
     homepage: homepage_en,
+    company_angel: company_angel_en,
+    company_demon: company_demon_en,
+    person_angel: person_angel_en,
+    person_demon: person_demon_en,
    },
    fr: {
     homepage: homepage_fr,
+    company_angel: company_angel_fr,
+    company_demon: company_demon_fr,
+    person_angel: person_angel_fr,
+    person_demon: person_demon_fr,
    },
    de: {
     homepage: homepage_de,
+    company_angel: company_angel_de,
+    company_demon: company_demon_de,
+    person_angel: person_angel_de,
+    person_demon: person_demon_de,
    }
   },
  })
